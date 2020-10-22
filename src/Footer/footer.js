0fc3c4748b177128c9e577145196714730794b9b
Amelie Boudier <amelie.boudier01@gmail.com>
2020-10-22 19:48:18 +0200

    added target blank to footer links

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -7,8 +7,8 @@ class Footer extends Component {
       <div className="footer">
         <div>Made by Amélie Boudier for Accenture's coding challenge</div>
         <div className="links">
-          <a href="https://github.com/ameliebou/my-accenture-react"><i class="fab fa-github-square"></i></a>
-          <a href="https://www.linkedin.com/in/amelie-boudier/"><i class="fab fa-linkedin"></i></a>
+          <a href="https://github.com/ameliebou/my-accenture-react" target="_blank" rel="noopener noreferrer"><i class="fab fa-github-square"></i></a>
+          <a href="https://www.linkedin.com/in/amelie-boudier/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
           <a href="#banner"><i class="fas fa-chevron-circle-up" id="scroll-up"></i></a>
         </div>
       </div>
