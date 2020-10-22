33079a528be8d7b9be255e5d9fac54b8dc055e30
Amelie Boudier <amelie.boudier01@gmail.com>
2020-10-22 12:04:55 +0200

    improved Badge component

--- src/Badge/badge.js
+++ src/Badge/badge.js
@@ -2,11 +2,20 @@ import React, { Component } from 'react';
 import './badge.css';
 
 class Badge extends Component {
+
   render() {
 
+    let description = null;
+
+    const style = {
+      backgroundImage: `url('${this.props.picture}')`
+    };
+
     return (
-      <div className="badge">
-        <h2>{this.props.title}</h2>
+      <div>
+        <div className="badge" style={style}>
+          <h2>{this.props.title}</h2>
+        </div>
       </div>
     )
   }
