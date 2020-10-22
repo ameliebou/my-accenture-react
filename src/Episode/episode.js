17c28c442ccf59342ac1c708caf6c038c44a3a90
Amelie Boudier <amelie.boudier01@gmail.com>
2020-10-22 12:21:08 +0200

    added toggle on videos

--- src/Episode/episode.js
+++ src/Episode/episode.js
@@ -2,15 +2,33 @@ import React, { Component } from 'react';
 import './episode.css';
 
 class Episode extends Component {
+  state = {
+    showVideo: false
+  };
+
+  playVideo = () => {
+    const doesShow = this.state.showVideo;
+    this.setState({showVideo: !doesShow});
+  };
+
   render() {
+    let video = null;
+
+    if (this.state.showVideo) {
+      const videoUrl = this.props.url
+      video = (
+        <iframe src={videoUrl} controls></iframe>
+      )
+    }
+
     const style = {
       backgroundImage: `url('${this.props.image}')`
     };
-    const videoUrl = this.props.url
     return (
-      <div className="card-episode">
-        <iframe src={videoUrl} controls></iframe>
+      <div className="card-episode" style={style} onClick={this.playVideo}>
+
         <p className="card-text">{this.props.title}</p>
+        {video}
       </div>
     )
   }
