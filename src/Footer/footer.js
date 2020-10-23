4cc5195f54450f15f1da137a48f35c665aace778
Amelie Boudier <amelie.boudier01@gmail.com>
2020-10-23 10:17:53 +0200

    adapted footer to mobile

--- src/Footer/footer.js
+++ src/Footer/footer.js
@@ -5,7 +5,7 @@ class Footer extends Component {
   render() {
     return(
       <div className="footer">
-        <div>Made by Amélie Boudier for Accenture's coding challenge</div>
+        <div className="footer-line">Made by Amélie Boudier for Accenture's coding challenge</div>
         <div className="links">
           <a href="https://github.com/ameliebou/my-accenture-react" target="_blank" rel="noopener noreferrer"><i class="fab fa-github-square"></i></a>
           <a href="https://www.linkedin.com/in/amelie-boudier/" target="_blank" rel="noopener noreferrer"><i class="fab fa-linkedin"></i></a>
